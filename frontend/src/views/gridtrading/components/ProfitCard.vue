f7cfde2a6d78aa19d7b69dbe285bf488042ecd12
charleswu <2471731988@qq.com>
2026-04-02 01:26:24 +0800

    fix: 修复 ProfitCard.vue 导入错误

--- frontend/src/views/gridtrading/components/ProfitCard.vue
+++ frontend/src/views/gridtrading/components/ProfitCard.vue
@@ -29,7 +29,8 @@
 </template>
 
 <script setup>
-import { formatAmount, computed } from 'vue'
+import { computed } from 'vue'
+import { formatAmount } from '@/utils/format'
 
 const props = defineProps({
   totalMarketValue: {
